d94b9af1eac4a08609898afb3860bd7c3ac6fab2
Fatolu Stephen <smjssp@gmail.com>
2023-04-03 00:03:37 +0100

    Ready to deploy

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,7 +42,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     }
 
@@ -63,30 +63,36 @@ const ExpenseForm = (props) => {
             value={enteredTitle}
             onChange={titleChangeHandler}
           />
-          <div className='new-expense__control'>
-            <label>Amount</label>
-            <input
-              type='number'
-              min='0.01'
-              step='0.01'
-              value={enteredAmount}
-              onChange={amountChangeHandler}
-            />
-          </div>
-          <div className='new-expense__control'>
-            <label>Date</label>
-            <input
-              type='date'
-              min='2019-01-01'
-              max='2022-12-31'
-              value={enteredDate}
-              onChange={dateChangeHandler}
-            />
-          </div>
         </div>
-      </div>
-      <div className='new-expense__actions'>
-        <button type='submit'> Add Expense</button>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+        <div className='new-expense__actions'>
+          <button type='button' onClick={props.onCancel}>
+            Cancel
+          </button>
+          <button type='submit'>
+            {' '}
+            Add <br /> Expense
+          </button>
+        </div>
       </div>
     </form>
   )
